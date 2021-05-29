5ee58ab6929dce30cdf11362a88bf5c592be67ed
unknown <matt.mcquain@gmail.com>
2021-05-28 19:51:58 -0700

    deploying to heroku

---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,103 @@
+import React, { useState } from 'react';
+import { Container, Grow, Grid, Paper, AppBar, TextField, Button } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+import { useHistory, useLocation } from 'react-router-dom';
+import ChipInput from 'material-ui-chip-input';
+
+import { getPostsBySearch } from '../../actions/posts';
+import Posts from '../Posts/Posts';
+import Form from '../Form/Form';
+import Pagination from '../Pagination/Pagination';
+
+import useStyles from './styles';
+
+
+// urlSearchParams helps us know what page we are on and what search term we are looking for. We can use it as a hook 
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
+
+
+const Home = () => {
+  const [currentId, setCurrentId] = useState(0);
+  const dispatch = useDispatch();
+  // query is where we are getting our page info from
+  const query = useQuery();
+  const history = useHistory();
+  // reads our URL and sees if we have a page parameter in there. If so, it populates the page variable. Else, if we dont have the page, we must be on the first one
+  const page = query.get('page') || 1;
+  const searchQuery = query.get('searchQuery');
+  const classes = useStyles();
+  const [search, setSearch] = useState('');
+  const [tags, setTags] = useState([]);
+
+  const searchPost = () => {
+    //trim just makes sure there are no empty spaces
+    if(search.trim() || tags) {
+      // dispatch -> fetch search post
+      //we join them with a comma just incase we have an array we can turn things into a string, which lets us easily pass data from front to backend.
+      dispatch(getPostsBySearch({ search, tags: tags.join(',') }));
+
+      //Client Side Routing: this will send users to the correct URL after they have searched for a tag or memory.
+      //the search is coming from state, we use 'none' string if there is no search, the & symbol adds the tags. We use the ',' because we have to split then join again.
+      history.push(`/posts/search?searchQuery=${search || 'none'}&tags=${tags.join(',')}`);
+    } else {
+      //we redirect back because we've searched for nothing
+      history.push('/');
+    }
+  };
+
+  const handleKeyPress = (e) => {
+    //keyCode 13 is the Enter key
+    if (e.keyCode === 13) {
+      searchPost();
+    }
+  };
+
+  const handleAddChip = (tag) => setTags([...tags, tag]);
+
+  const handleDeleteChip = (chipToDelete) => setTags(tags.filter((tag) => tag !== chipToDelete));
+
+  return (
+    <Grow in>
+      <Container maxWidth="xl">
+        <Grid container justify="space-between" alignItems="stretch" spacing={3} className={classes.gridContainer}>
+          <Grid item xs={12} sm={6} md={9}>
+            <Posts setCurrentId={setCurrentId} />
+          </Grid>
+          <Grid item xs={12} sm={6} md={3}>
+            <AppBar className={classes.appBarSearch} position="static" color="inherit" >
+              <TextField 
+                name="search"
+                variant="outlined"
+                label="Search Memories"
+                onKeyPress={handleKeyPress}
+                fullWidth
+                value={search}
+                // this allows us to change the value of the search
+                onChange={(e) => setSearch(e.target.value)}
+              />
+              <ChipInput 
+                style={{ margin: '10px 0'}}
+                value={tags}
+                onAdd={(chip) => handleAddChip(chip)}
+                onDelete={(chip) => handleDeleteChip(chip)}
+                label="Search Tags"
+                variant="outlined"
+              />
+              <Button onClick={searchPost} className={classes.searchButton} variant="contained" color="primary">Search</Button>
+            </AppBar>
+            <Form currentId={currentId} setCurrentId={setCurrentId} />
+            {( !searchQuery && !tags.length) && (
+              <Paper className={classes.pagination} elevation={6} className={classes.pagination}>
+                <Pagination page={page} />
+              </Paper>
+            )}
+          </Grid>
+        </Grid>
+      </Container>
+    </Grow>
+  );
+};
+
+export default Home;
---
+++ server/controllers/posts.js
@@ -0,0 +1,115 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+import PostMessage from '../models/postMessage.js';
+
+const router = express.Router();
+
+export const getPosts = async (req, res) => { 
+    const { page } = req.query;
+
+    try {
+        const LIMIT = 8;
+        const startIndex = (Number(page) - 1) * LIMIT; //gets the starting index of every page 
+        const total = await PostMessage.countDocuments({});
+
+        const posts = await PostMessage.find().sort({ _id: -1 }).limit(LIMIT).skip(startIndex);
+                
+        res.json({ data: posts, currentPage: Number(page), numberOfPages: Math.ceil(total / LIMIT) });
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
+
+// QUERY -> /posts?page=1 -> page = 1. Use query to query/search for some data
+// PARAMS -> /posts/:id -> id = xxx. use params if we want to get some specific resource, like posts/:id of post
+
+export const getPostsBySearch = async (req, res) => {
+    const { searchQuery, tags } = req.query;
+    try {
+        //'i' stands for 'ignore case', so it wont matter if the search term is upper/lowercase
+        //we are converting it to a RegExp because its easier for mongoDB and Mongoose to search the database
+        const title = new RegExp(searchQuery, 'i');
+
+        //the $or just means find me the title OR the tags. ELI5: find me all the posts that match either the title, or is one of the 'tags:' (in the array of tags) equal to our 'tags'
+        const posts = await PostMessage.find({ $or: [ { title }, { tags: { $in: tags.split(',') } }] });
+
+        res.json({ data: posts });
+    } catch (error) {
+        res.status(404).json({ message: error.message })
+    }
+}
+
+export const getPost = async (req, res) => { 
+    const { id } = req.params;
+
+    try {
+        const post = await PostMessage.findById(id);
+        
+        res.status(200).json(post);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
+
+export const createPost = async (req, res) => {
+    const post = req.body;
+
+    const newPostMessage = new PostMessage({ ...post, creator: req.userId, createdAt: new Date().toISOString() })
+
+    try {
+        await newPostMessage.save();
+
+        res.status(201).json(newPostMessage );
+    } catch (error) {
+        res.status(409).json({ message: error.message });
+    }
+}
+
+export const updatePost = async (req, res) => {
+    const { id } = req.params;
+    const { title, message, creator, selectedFile, tags } = req.body;
+    
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+
+    const updatedPost = { creator, title, message, tags, selectedFile, _id: id };
+
+    await PostMessage.findByIdAndUpdate(id, updatedPost, { new: true });
+
+    res.json(updatedPost);
+}
+
+export const deletePost = async (req, res) => {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+
+    await PostMessage.findByIdAndRemove(id);
+
+    res.json({ message: "Post deleted successfully." });
+}
+
+export const likePost = async (req, res) => {
+    const { id } = req.params;
+
+    if (!req.userId) {
+        return res.json({ message: "Unauthenticated" });
+      }
+
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+    
+    const post = await PostMessage.findById(id);
+
+    const index = post.likes.findIndex((id) => id ===String(req.userId));
+
+    if (index === -1) {
+      post.likes.push(req.userId);
+    } else {
+      post.likes = post.likes.filter((id) => id !== String(req.userId));
+    }
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, post, { new: true });
+    res.status(200).json(updatedPost);
+}
+
+
+export default router;
